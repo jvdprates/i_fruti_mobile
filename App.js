2ea22eb61bfd10ad1b473b03ac35a4a022775eb6
Beto <zerobertodavid@gmail.com>
2020-07-11 20:25:09 -0300

    configurando ambiente
    instalação

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.teste}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +18,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  teste: {
+    fontSize: 30,
+  }
 });
