9afe997ca172ae06bc4e824ae564787230dd4946
João Prates <jvdprates@hotmail.com>
2020-08-05 22:24:50 -0300

    Tentando colocar Webview

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.teste}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView source={{uri: 'https://ifruti.com.br'}}/>
     </View>
   );
 }
